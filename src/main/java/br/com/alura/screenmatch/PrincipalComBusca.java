c0b0db287026726202d37b38048e30e47cb0866c
Celso Ricardo <celsopixain@gmail.com>
2024-06-05 16:08:12 -0500

    Início projeto Spring

---
+++ src/main/java/br/com/alura/screenmatch/PrincipalComBusca.java
@@ -0,0 +1,16 @@
+package br.com.alura.screenmatch;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class PrincipalComBusca implements CommandLineRunner {
+    public static void main(String[] args) {
+        SpringApplication.run(ScreenmatchApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Consumo API");
+    }
+}
